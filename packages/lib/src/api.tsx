7d09ebfeac6659fdeddbc6ff6f6a89f53db1392b
aric.zheng <1290657123@qq.com>
2025-10-20 13:30:34 +0800

    feat: add api compnent

---
+++ packages/lib/src/api.tsx
@@ -0,0 +1,58 @@
+import React, { FC, useEffect } from 'react';
+import ReactAntResourceForm, { ReactAntResourceFormProps } from '.';
+import { Form, message } from 'antd';
+
+type ReactAntResourceFormApiProps = ReactAntResourceFormProps & {
+  context: any;
+  lang?: string;
+  params?: Record<string, any>;
+};
+
+const locales = {
+  'zh-CN': {
+    create_success: '创建成功',
+    update_success: '更新成功',
+  },
+  'en-US': {
+    create_success: 'Create success',
+    update_success: 'Update success',
+  },
+};
+
+const defaultProps = {
+  lang: 'zh-CN',
+};
+
+const ReactAntResourceFormApi: FC<ReactAntResourceFormApiProps> = (props) => {
+  const { name, context, params, lang, ...rest } = { ...defaultProps, ...props };
+  const resourceEdit = `${name}_update`;
+  const resourceCreate = `${name}_create`;
+  const resourceShow = `${name}_show`;
+  const [form] = Form.useForm();
+  const isEdit = Boolean(params?.id);
+  const t = (key: string) => locales[lang!][key];
+  const handleFinish = (values) => {
+    if (isEdit) {
+      context[resourceEdit]({ id: params!.id, ...values }).then((res) => {
+        message.success(t('update_success'));
+      });
+    } else {
+      context[resourceCreate](values).then((res) => {
+        message.success(t('create_success'));
+      });
+    }
+  };
+
+  // init detail
+  useEffect(() => {
+    if (isEdit) {
+      context[resourceShow]({ id: params!.id }).then((res) => {
+        form.setFieldsValue(res);
+      });
+    }
+  }, [isEdit]);
+
+  return <ReactAntResourceForm form={form} onFinish={handleFinish} {...rest} />;
+};
+
+export default ReactAntResourceFormApi;
--- packages/lib/src/main.tsx
+++ packages/lib/src/main.tsx
@@ -1,3 +1,5 @@
 import ReactAntResourceForm from '.';
+import type { ReactAntResourceFormProps } from '.'
 
 export default ReactAntResourceForm;
+export type { ReactAntResourceFormProps };
